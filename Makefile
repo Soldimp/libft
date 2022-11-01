87b611c2ed25ddfc75b2706e7242f0bd07697ca2
Nuno Manuel Garcia Guerreiro <nugarcia@c1r17s13.42lisboa.com>
2022-11-01 11:41:28 +0000

    ft_putnbr_fd pass franci

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: nugarcia < nugarcia@student.42lisboa.co    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/26 11:30:23 by nugarcia          #+#    #+#              #
-#    Updated: 2022/11/01 11:06:07 by nugarcia         ###   ########.fr        #
+#    Updated: 2022/11/01 11:08:25 by nugarcia         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,7 +15,7 @@ SRC = ft_memset ft_bzero ft_calloc ft_memcpy ft_memmove ft_memchr\
 ft_memcmp ft_strlen ft_strlcpy ft_strlcat ft_strchr ft_strrchr\
 ft_atoi ft_isalpha ft_isdigit ft_isalnum ft_strncmp ft_strnstr\
 ft_isascii ft_isprint ft_toupper ft_tolower ft_strdup ft_putstr_fd\
-ft_putchar_fd ft_putendl_fd\
+ft_putchar_fd ft_putendl_fd ft_putnbr_fd\
 
 CC =gcc
 RM =rm -f
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: nugarcia < nugarcia@student.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 16:12:30 by nugarcia          #+#    #+#             */
-/*   Updated: 2022/10/31 18:11:40 by nugarcia         ###   ########.fr       */
+/*   Updated: 2022/11/01 11:39:18 by nugarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,29 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	unsigned int	nbr;
+	char	nbr;
 
-	nbr = n;
-	if (nbr == )
+	nbr = n + 48;
+	if (n >= INT_MIN && n <= INT_MAX)
 	{
-
+		if (n == INT_MIN)
+		{
+			write (fd, "-", 1);
+			write (fd, "2", 1);
+			write (fd, "147483648", 9);
+		}
+		else if (n < 0)
+		{
+			n = -n;
+			write (fd, "-", 1);
+			ft_putnbr_fd(n, fd);
+		}
+		else if (n > 9)
+		{
+			ft_putnbr_fd((n / 10), fd);
+			ft_putnbr_fd((n % 10), fd);
+		}
+		else
+			write(fd, &nbr, 1);
 	}
 }
